3ea867c01615631b273f9867fa93b03a340feeca
nikita <zotiken@gmail.com>
2019-09-21 23:00:19 +0300

    module3-task3

---
+++ js/main.js
@@ -0,0 +1,114 @@
+'use strict';
+
+var templatePicture = document.querySelector('#picture')
+  .content.
+querySelector('.picture');
+
+document.querySelector('.big-picture').classList.remove('hidden');
+document.querySelector('.social__comment-count').classList.add('visually-hidden');
+document.querySelector('.comments-loader').classList.add('visually-hidden');
+
+var bigPictureImg = document.querySelector('.big-picture__img');
+
+var descriptionFotos = [];
+var descriptions = [
+  'Шедеврально!)',
+  'Full shit',
+  'Бездарность,отрубить руки!'
+];
+
+var comments = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var names = [
+  'Артем',
+  'Егор',
+  'Timati',
+  'Sam',
+  'Gogi',
+  'Мифодий'
+];
+
+// --------- рандомное елемент массива -----------
+
+
+var makeRandomValue = function (array) {
+  return Math.round(Math.random() * (array.length - 1));
+};
+
+// --------- рандомное число с min и  max -----------
+
+
+var randomInteger = function (min, max) {
+  // получить случайное число от min до max
+  var rand = min + Math.random() * (max - min);
+  return Math.round(rand);
+};
+
+// --------- генерация коментариев -----------
+
+var commitsGeneration = function (params) {
+  var arr = [];
+  for (var i = 0; i < params; i++) {
+    arr.push({
+      avatar: 'img/avatar-' + randomInteger(1, 6) + '.svg',
+      message: comments[makeRandomValue(comments)],
+      name: names[makeRandomValue(names)]
+    });
+  }
+  return arr;
+};
+
+// --------- генерация описания фото -----------
+
+var generateDescriptionFoto = function (number) {
+  for (var e = 1; e <= number; e++) {
+    descriptionFotos.push({
+      url: 'photos/' + e + '.jpg',
+      description: descriptions[makeRandomValue(descriptions)],
+      likes: randomInteger(15, 200),
+      comments: commitsGeneration(2)
+    });
+  }
+};
+generateDescriptionFoto(25);
+
+var pictureBlokGeneration = function (params) {
+
+  var pictureElement = templatePicture.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = params.url;
+  pictureElement.querySelector('.picture__likes').textContent = params.likes;
+  pictureElement.querySelector('.picture__comments').textContent = params.comments.length;
+
+  return pictureElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var r = 0; r < descriptionFotos.length; r++) {
+  fragment.appendChild(pictureBlokGeneration(descriptionFotos[r]));
+}
+document.querySelector('.pictures').appendChild(fragment);
+
+
+var bigPictureBlokGeneration = function (params) {
+  document.querySelector('.likes-count').textContent = params.likes;
+  bigPictureImg.querySelector('img').src = params.url;
+  document.querySelector('.comments-count').textContent = params.comments.length;
+  var commentsList = document.querySelector('.social__comments').querySelectorAll('.social__comment');
+  for (var o = 0; o < commentsList.length; o++) {
+    commentsList[o].querySelector('img').src = 'img/avatar-' + randomInteger(1, 6) + '.svg';
+    commentsList[o].querySelector('img').alt = params.comments[o].name;
+    commentsList[o].querySelector('.social__text').textContent = params.comments[o].message;
+  }
+  document.querySelector('.social__caption').textContent = params.description;
+};
+
+bigPictureBlokGeneration(descriptionFotos[randomInteger(0, 25)]);
+
+
